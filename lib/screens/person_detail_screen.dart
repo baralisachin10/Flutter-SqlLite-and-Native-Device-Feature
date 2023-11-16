c728c58ddb0d1cada024c676b2636121f89c82d1
sachin barali <sachinbarali41@gmail.com>
2023-11-16 13:45:00 +0545

    details screen

--- lib/screens/person_detail_screen.dart
+++ lib/screens/person_detail_screen.dart
@@ -15,6 +15,59 @@ class PersonDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(person.name),
       ),
+      body: Stack(
+        children: [
+          Image.file(
+            person.image,
+            height: double.infinity,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            left: 0,
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                height: 50,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.onBackground,
+                    borderRadius: BorderRadius.circular(12)),
+                child: Row(
+                  children: [
+                    // profession
+                    Row(
+                      children: [
+                        const Icon(Icons.work),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          person.profession,
+                        ),
+                      ],
+                    ),
+                    const Spacer(),
+                    //contact number
+                    Row(
+                      children: [
+                        const Icon(Icons.phone),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Text(person.phoneNumber),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
